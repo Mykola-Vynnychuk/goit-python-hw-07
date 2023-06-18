14f841b36ab8f03d9135cac0cd8966227da58943
Микола Винничук <62299632+Mykola-Vynnychuk@users.noreply.github.com>
2023-06-19 00:32:39 +0300

    add URL in setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -4,7 +4,7 @@ setup(
     name='clean_folder',
     version='1.0',
     description='sorts files and extracts archives',
-    url='https://github.com/Mykola-Vynnychuk/goit-python-hw-07',
+    url='https://github.com/Mykola-Vynnychuk/goit-python-hw-07/tree/main/clean_folder',
     author='Mykola Vynnychuk',
     author_email='nik4779@gmail.com',
     license='MIT',
